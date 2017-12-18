90abe6907b6fefbd6ec373307446775b1509b013
Bilhasry Ramadhony <bilhasry@gmail.com>
2017-12-18 08:58:50 +0800

    update

--- MoE/assets/js/app.js
+++ MoE/assets/js/app.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
 
-	setTimeout(function(){
-		$('.loader-overlay').hide();
-	}, 2000);
-
 	var data_list 	= new Array();
 	var data 		= document.getElementById('data');
 	var up 			= true;
